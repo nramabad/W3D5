82b89dc5349e0ffe55b48b6af20e448e09480ee1
nramabad <nramabad@gmail.com>
2018-11-11 15:08:30 -0800

    Finalize solo work

---
+++ Active Record/lib/00_attr_accessor_object.rb
@@ -0,0 +1,14 @@
+class AttrAccessorObject
+  def self.my_attr_accessor(*names)
+    names.each do |name|
+      define_method(name) do
+        puts "defining a getter"
+        instance_variable_get("@#{name}")
+      end
+      define_method("#{name}=") do |value|
+        puts "defining a setter"
+        instance_variable_set("@#{name}", value)
+      end
+    end
+  end
+end
---
+++ Active Record/lib/01_sql_object.rb
@@ -0,0 +1,78 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    return @cols if @cols
+    @cols = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      LIMIT
+        0
+    SQL
+
+    @cols.map!(&:to_sym)
+  end
+
+  def self.finalize!
+
+    self.columns.each do |col|
+      define_method(col) do
+        self.attributes(col)
+      end
+      # define_method("#{col}=") do |value|
+      #   self.attributes(col) = value
+      # end
+    end
+
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.table_name
+    @table_name ||= "#{self}".tableize
+    @table_name
+  end
+
+  def self.all
+    # ...
+  end
+
+  def self.parse_all(results)
+    # ...
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    # ...
+  end
+
+  def attributes
+    @attributes ||= {}
+  end
+
+  def attribute_values
+    # ...
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
